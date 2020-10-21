e4c6d4bae4965de2a6034815c857d8b7b86ffcfa
Wesley Santos <wesley.sluk@hotmail.com>
2020-10-21 15:48:58 -0300

    Instancia de classes, atributos e metodos Co-authored-by: Ciro Costa <ciro.fga@gmail.com> Co-authored-by: Ana Clara C. Oliveira <anac.ijuma@gmail.com>

---
+++ Republica/src/entidades/Subcategoria.java
@@ -0,0 +1,22 @@
+package entidades;
+
+public class Subcategoria {
+	
+	private String descricaoSubcategoria;
+
+	public Subcategoria(String descricaoSubcategoria) {
+		super();
+		this.descricaoSubcategoria = descricaoSubcategoria;
+	
+	}
+
+	public String getDescricaoSubcategoria() {
+		return descricaoSubcategoria;
+	}
+
+	public void setDescricaoSubcategoria(String descricaoSubcategoria) {
+		this.descricaoSubcategoria = descricaoSubcategoria;
+	}
+	
+	
+}
