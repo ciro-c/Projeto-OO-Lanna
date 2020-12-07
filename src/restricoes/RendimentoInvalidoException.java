99499592734d794809b5dd60115bab5af2a181c8
Ciro Costa <54088490+ciro-c@users.noreply.github.com>
2020-12-07 01:45:05 -0300

    Arrumando os arquivos

---
+++ src/restricoes/RendimentoInvalidoException.java
@@ -0,0 +1,13 @@
+package restricoes;
+
+public class RendimentoInvalidoException extends Exception {
+	
+	public RendimentoInvalidoException() {
+		super(); 
+	}
+	
+	public RendimentoInvalidoException(String msg) {
+		super(msg);
+	}
+
+}
