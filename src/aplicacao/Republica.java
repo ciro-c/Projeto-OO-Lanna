8a8327d9bc3a328750e3e734e6ddbb450a585802
Ciro Costa <54088490+ciro-c@users.noreply.github.com>
2020-12-11 20:25:30 -0300

    Arrumando o main para gerar os arquivos de pessoas e do despesas
    Co-authored-by: Wesley P. dos Santos <wesley.sluk@hotmail.com>
    Co-authored-by: Ana Clara C. Oliveira <anac.ijuma@gmail.com>

--- src/aplicacao/Republica.java
+++ src/aplicacao/Republica.java
@@ -36,7 +36,7 @@ public class Republica {
 		do {
 		
 			JOptionPane.showMessageDialog(null, "----------DIVISO DAS DESPESAS DE UMA REPBLICA----------\n\nOlá, o que pretende fazer?\n"
-			+ "1) Cadastrar pessoa.\n2) Cadastrar despesa.\n3) Despesas cadastradas.\n4) Pessoas cadastradas. \n5) Sair do Programa");
+			+ "1) Cadastrar pessoa.\n2) Cadastrar despesa.\n3) Despesas cadastradas.\n4) Pessoas cadastradas. \n5) Gerar relatorio\n6) Sair do Programa");
 			String respostaOpcao = JOptionPane.showInputDialog("Digite o nº da opção desejada");
 			int rO = Integer.parseInt(respostaOpcao);
 			i = rO ;
@@ -44,7 +44,7 @@ public class Republica {
 			// Opes
 			
 			switch(rO) {
-				// Cadastrar pessoa
+
 				case 1:
 					
 					boolean cadastroUm;
@@ -61,22 +61,25 @@ public class Republica {
 							String rendimentoPessoa = JOptionPane.showInputDialog("Rendimento da pessoa: "); 
 								
 							try {
-								
-								p.add(new Pessoa(nomePessoa, emailPessoa, rendimentoPessoa));
-								
-								int ultimoObjeto = p.size()-1;
-								
-								if ((nomePessoa == null || nomePessoa.isEmpty()) || (emailPessoa == null || emailPessoa.isEmpty()) || (rendimentoPessoa == null ||
-								rendimentoPessoa.isEmpty())) {
-									p.remove(ultimoObjeto);
+																
+								if ((nomePessoa == null || nomePessoa.isEmpty()) ||
+								    (emailPessoa == null || emailPessoa.isEmpty()) ||
+								    (rendimentoPessoa == null || rendimentoPessoa.isEmpty())) 
+								{
+									
 									throw new DadosPessoaisIncompletosException("Alguns dados não foram inseridos.");
-								}else if(Double.parseDouble(rendimentoPessoa) < 0) {
-									p.remove(ultimoObjeto);
+								
+								}else if(Double.parseDouble(rendimentoPessoa) < 0) 
+								{
 									throw new RendimentoInvalidoException("Não é possível inserir valores negativos.");
-								}else if(!rendimentoPessoa.matches("^(\\d+(\\.\\d{0,2})?|\\.?\\d{1,2})$")){
-									p.remove(ultimoObjeto);
+								
+								}else if(!rendimentoPessoa.matches("^(\\d+(\\.\\d{0,2})?|\\.?\\d{1,2})$"))
+								{
+								
 									throw new NumberFormatException("Não é possível inserir caracteres especiais ou letras.");
+								
 								}else {
+									p.add(new Pessoa(nomePessoa, emailPessoa, rendimentoPessoa));
 									JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
 									JOptionPane.showMessageDialog(null, "Número de cadastros efetuados: " +p.size());
 									cadastroUm = true;
@@ -103,49 +106,49 @@ public class Republica {
 							
 				break;
 				
-				// Cadastrar despesa	
+	
 				case 2:
-					String descricaoSubcategoria;
+					String descricaoSubcategoria = "";
 					String registro = JOptionPane.showInputDialog("Digite a quantidade de despesas existentes: "); 
 					int k = Integer.parseInt(registro);
 
 					
 					for(int m=0; m<k; m++) {
+						
 						JOptionPane.showMessageDialog(null, "Despesa #" + (m+1) + ": ");
 						String descricaoDespesa = JOptionPane.showInputDialog("Nome da despesa: Ex.(CAESB, CEB, Net..)");
 						String descricaoCategoria = JOptionPane.showInputDialog("Digite o nome da categoria. Ex.(Água, Luz, Telefonia e comunicações..)");
 						String valor = JOptionPane.showInputDialog("Digite o valor da despesa: "); 
 						double val = Double.parseDouble(valor);
 						int sub = JOptionPane.showConfirmDialog(null, "Deseja cadastrar uma subcategoria?", "Continua", JOptionPane.YES_NO_OPTION);
-						
-						list.add(new Despesas(descricaoDespesa,val));
-						
-						
-						int ultimaDespesa = list.size()-1;
-						
-						list.get(ultimaDespesa).cadastrarCategoria(descricaoCategoria);
-						
+											
 						if(sub == JOptionPane.YES_OPTION) {
 							descricaoSubcategoria = JOptionPane.showInputDialog("Digite o nome da subcategoria relacionada a despesa.");
-							list.get(ultimaDespesa).categoria.cadastrarSubcategoria(descricaoSubcategoria);
 						}
 						try {
 							
 							if ((descricaoDespesa == null || descricaoDespesa.isEmpty()) || (valor == null ||valor.isEmpty())) {
-								list.remove(ultimaDespesa);
+								
 								throw new DespesaInvalidaException("Alguns dados nao foram inseridos na despesa.");
+							
 							}else if(Integer.parseInt(valor) < 0) {
-								list.remove(ultimaDespesa);
+							
 								throw new ValorDespesaInvalidaException("No  possvel inserir valores negativos.");
+							
 							}else if ( (descricaoCategoria == null || descricaoCategoria.isEmpty()) ) {
-								list.remove(ultimaDespesa);
+							
 								throw new CategoriaInvalidaException("Nome da categoria nao inserido");
+							
 							}else {
+							
+								list.add(new Despesas(descricaoDespesa,val));
+								int ultimaDespesa = list.size()-1;
+								list.get(ultimaDespesa).cadastrarCategoria(descricaoCategoria);
+								list.get(ultimaDespesa).categoria.cadastrarSubcategoria(descricaoSubcategoria);
 								JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
 								JOptionPane.showMessageDialog(null, "Nmero de cadastros efetuados: " +list.size());
 							}
 							
-	
 						} catch( ValorDespesaInvalidaException | DespesaInvalidaException | CategoriaInvalidaException e) {
 							String msg = e.getMessage() + " Exceo capturada!";
 							JOptionPane.showMessageDialog(null, msg);
@@ -153,7 +156,7 @@ public class Republica {
 						}
 					}
 				break;
-				// Cadastrar categoria
+
 				case 3:
 					JOptionPane.showMessageDialog(null, "Quantidade de despesas cadastradas:" + list.size());
 					String desp = JOptionPane.showInputDialog("Digite a despesa que deseja procurar: "); 
@@ -169,16 +172,19 @@ public class Republica {
 
 					
 				break;
-				// Sair do programa
 				case 4:
 					JOptionPane.showMessageDialog(null, "Quantidade de despesas cadastradas:" + p.size());
 				break;
 				
 				case 5:
+					JOptionPane.showMessageDialog(null, "Gerar relatorio");
+				break;
+				
+				case 6:
 					JOptionPane.showMessageDialog(null, "Encerrando o programa...");
 				break;
 			}
-		}while(i!=4);
+		}while(i!=6);
 		
 		/*public static boolean hasDespesa(List<Despesas> list, String descricaoDespesa) {
 			Despesas empl = list.stream().filter(x -> x.getDescricaoDespesas() == descricaoDespesas).findFirst().orElse(null));
